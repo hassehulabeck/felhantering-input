0b59f80528e5c8d375d646da696d9ad89a24f7cb
hassehulabeck <hansbudandersson@gmail.com>
2019-10-28 09:53:44 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    var input = document.getElementById("age");
+    var knapp = document.getElementsByTagName("button");
+    var message = document.getElementById("message");
+
+    knapp[0].addEventListener("click", () => {
+        var age = input.value;
+
+        // Räkna ut vilket år som användaren var halva sin ålder.
+        var year = new Date().getFullYear();
+
+        // Värden från input är alltid string, även om input-typen är number.
+        console.log(typeof (age));
+
+        // Blandar string och number för att "tvinga" fram ett fel.
+        var whatYear = "User is " + year - (Math.floor(age / 2));
+
+        message.innerText = whatYear;
+    })
+
+})
